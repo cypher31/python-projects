2a2a8c0093a4b9465dcb8322631c40f97cb2d3d7
Kelby Styler <kelbystyler@gmail.com>
2022-03-19 16:51:55 -0700

    Countdown python program. Dealt mainly with While loops and the divmod function.

--- intro.py
+++ intro.py
@@ -1,1 +1,7 @@
-Text
+#Tutorial sources and general goal
+
+#Tutorials picked from the 25 topis from this list:
+#https://www.freecodecamp.org/news/python-projects-for-beginners/
+
+#starting with
+#Coutdown timer
